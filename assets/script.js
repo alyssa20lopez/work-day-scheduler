fce0151bcb730c6a4ea2ff2f8d7caa982a4d2a79
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-08-25 18:32:08 -0400

    Pushing to Local Storage

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,11 @@
 var currentDay = $('#currentDay').html("" + moment().format("dddd, MMMM Do"));
 
+function store(){
+    var input = document.getElementById('description').value;
+
+    const container = {
+        input: input,
+    }
+
+    window.localStorage.setItem(description,JSON.stringify(container));
+}
