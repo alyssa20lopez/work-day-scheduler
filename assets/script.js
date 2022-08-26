412edce1d36cb51a22cc9390456110cbdf34fd0b
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-08-25 23:41:22 -0400

    Added Items to Local Storage

--- assets/script.js
+++ assets/script.js
@@ -4,11 +4,16 @@ function colorHours() {
     var currentTime = moment().format("HH");
 
     for (var i = 9; i <= 17; i++) {
-        if (i < currentTime) $(`#data${i}`).addclass("past");
-        if (i === currentTime) $(`#data${i}`).addclass("present");
-        if (i > currentTime) $(`#data${i}`).addclass("future");
+        if (i < currentTime)$(`#hour${i}`).addClass("past"); 
+        if (i == currentTime)$(`#hour${i}`).addClass("present");
+        if (i > currentTime)$(`#hour${i}`).addClass("future");
     }
-
-    colorHours();
 };
 
+$(document).ready(function () {
+    $(".saveBtn").on("click",function () {
+        var text = $(this).siblings(".text").val();
+
+        localStorage.setItem('description',text);
+    })
+});
