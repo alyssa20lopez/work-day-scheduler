0ce34d917aa55c7d5a72c344e2f93b22eac7b0e8
Alyssa20lopez <alyssa20lopez@gmail.com>
2022-08-26 19:49:29 -0400

    Updates to Local Storage and Time Block Functions

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,26 @@
 var currentDay = $('#currentDay').html("" + moment().format("dddd, MMMM Do"));
 
-function colorHours() {
-    var currentTime = moment().format("HH");
+
+    var currentTime = +moment().format("HH");
+
+    console.log(typeof currentTime);
 
     for (var i = 9; i <= 17; i++) {
+        console.log($(`#hour${i}`));
         if (i < currentTime)$(`#hour${i}`).addClass("past"); 
         if (i == currentTime)$(`#hour${i}`).addClass("present");
         if (i > currentTime)$(`#hour${i}`).addClass("future");
+
+        window.localStorage.setItem('description','#hour');
+        $('#hour').html(window.localStorage.getItem('description'));
     }
-};
 
 $(document).ready(function () {
     $(".saveBtn").on("click",function () {
-        var text = $(this).siblings(".text").val();
+        var text = $(this).prev().val();
+        var id = $(this).prev().attr('id');
+        
 
-        localStorage.setItem('description',text);
+        localStorage.setItem(id,text);
     })
 });
